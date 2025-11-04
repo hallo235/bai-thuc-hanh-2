df2853f6da1d4af4064680310cc0922625afb0b6
hallo235 <128238300+hallo235@users.noreply.github.com>
2025-11-04 22:16:58 +0700

    Update index.jsx

--- src/components/UserDetail/index.jsx
+++ src/components/UserDetail/index.jsx
@@ -1,24 +1,50 @@
-import React from "react";
-import {Typography} from "@mui/material";
+import React, { useEffect, useMemo } from "react";
+import { useParams, Link as RouterLink } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Stack,
+  Button,
+  Divider,
+} from "@mui/material";
+import models from "../../modelData/models";
 
-import "./styles.css";
-import {useParams} from "react-router-dom";
+const UserDetail = ({ setTopBarTitle }) => {
+  const { userId } = useParams();
+  const user = useMemo(() => models.userModel(userId), [userId]);
 
-/**
- * Define UserDetail, a React component of Project 4.
- */
-function UserDetail() {
-    const user = useParams();
-    return (
-        <>
-          <Typography variant="body1">
-            This should be the UserDetail view of the PhotoShare app. Since it is
-            invoked from React Router the params from the route will be in property match.
-            So this should show details of user: {user.userId}.
-            You can fetch the model for the user from models.userModel.
-          </Typography>
-        </>
-    );
-}
+  useEffect(() => {
+    if (user) setTopBarTitle?.(`${user.first_name} ${user.last_name}`);
+  }, [user, setTopBarTitle]);
+
+  if (!user) return <Typography sx={{ p: 2 }}>User not found.</Typography>;
+
+  return (
+    <Card variant="outlined" sx={{ m: 2 }}>
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          {user.first_name} {user.last_name}
+        </Typography>
+        <Divider sx={{ my: 1 }} />
+        <Stack spacing={0.5}>
+          <Typography><b>Location:</b> {user.location}</Typography>
+          <Typography><b>Occupation:</b> {user.occupation}</Typography>
+          <Typography><b>Description:</b> {user.description}</Typography>
+        </Stack>
+        <Stack direction="row" spacing={1} sx={{ mt: 2 }}>
+          <Button
+            variant="contained"
+            size="small"
+            component={RouterLink}
+            to={`/photos/${user._id}`}
+          >
+            View Photos
+          </Button>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default UserDetail;
